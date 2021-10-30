be001d9af42951effe874105c2aad67b88337d26
Агаларов Игорь <igorvippzone@gmail.com>
2021-10-30 09:11:26 +0300

    modal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
-$(function(){
+const faq = {
+  img: [
+    "images/FAQ/faq-1.jpg",
+    "images/FAQ/faq-2.jpg",
+    "images/FAQ/faq-1.jpg",
+    "images/FAQ/faq-1.jpg",
+    "images/FAQ/faq-1.jpg",
+    "images/FAQ/faq-1.jpg",
+  ],
+  title: ["Какие документы нужны для обучения?", "11111", "222222", "333333"],
+  description: ["000000", "11111", "222222", "333333"],
+};
 
+const faqBtn = document.querySelectorAll(".faq-cart__btn");
+
+faqBtn.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    const e = event.target.getAttribute("data-faq");
+    const modal = $.modal({
+      image: faq.img[e],
+      title: faq.title[e],
+      description: faq.description[e],
+    });
+    modal.open()
+  
+  });
 });
---
+++ js/modal.js
@@ -0,0 +1,58 @@
+$.modal = function (options) {
+  function _createModal(options) {
+    console.log(options)
+    const modal = document.createElement("div");
+    modal.classList.add("modal");
+    modal.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="modal__overlay" data-close="true">
+    <div class="modal__window">
+      <img src=${options.image} alt="" class="modal__img">
+        <h2 class="modal__title">${options.title}</h2>
+        <p class="modal__description">${options.description}</p>
+          <a href="#" class="modal__link">оставить заявку на обучение</a>
+    </div>
+  </div>`
+    );
+    document.body.appendChild(modal);
+    return modal;
+  }
+  const ANIMATION_SPEED = 300;
+  const $modal = _createModal(options);
+  let closing = false;
+  let destroyed = false;
+
+  const modal = {
+    open() {
+      if(destroyed){
+        console.log("destroyed")
+      }
+      !closing && $modal.classList.add("open");
+     },
+     close() {
+       closing = true;
+       $modal.classList.remove("open");
+       $modal.classList.add("hide");
+       setTimeout(() => {
+         $modal.classList.remove("hide");
+         closing = false;
+       }, ANIMATION_SPEED);
+     },
+  }
+$modal.addEventListener("click",(event)=>{
+
+ 
+     if(event.target.dataset.close){
+modal.close()
+     }
+
+ 
+})
+  return Object.assign(modal,{
+    destroy(){
+      $modal.parentNode.removeChild($modal)
+      destroyed=true
+    }
+  });
+};
+
